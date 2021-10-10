5eb7da825bbd034680d2e21c0f9cdabec0fd12c3
jorika <gogoladzeg321@gmail.com>
2021-10-10 23:05:34 +0400

    Initialize projects using Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
+import DataOfValutebi from './components/DataOfValutebi'
+const App =()=> {
+    const [dataOfValuts, setDataOfValuts]= useState([])
+    useEffect(()=>{
+        fetch('http://www.valutebi.ge/rates').then((response)=>{
+            return response.json()
+        }).then((data)=>{
+            setDataOfValuts(data)
+        })
+    },[])
+    const monacemebi = dataOfValuts.map((value,index)=>(
+        <DataOfValutebi
+         key={value.name}
+         ratesOfUSD={value.rates[index=0]}
+         rateOfEUR={value.rates[index=1]}
+         rateOfGBP={value.rates[index=2]}
+         rateOfTRY={value.rates[index=3]}
+         name={value.name}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        />
+
+        ))
+    console.log(dataOfValuts)
+    console.log(monacemebi)
+    return <ul>{monacemebi}</ul>
+    // return <div>hello</div>
 }
-
 export default App;
---
+++ src/components/DataOfValutebi.js
@@ -0,0 +1,8 @@
+// import classes from './DataOfValutebi.module.css'
+import React from 'react'
+const DataOfValutebi =(props)=> {
+    return <li>
+        <div>{props.name}</div>
+    </li>
+}
+export default DataOfValutebi;
